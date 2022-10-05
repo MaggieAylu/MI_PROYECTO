53c650d9ce760dad2c3cb33024c0c6c35d85fb06
Maggie D <magaayludiaz@gmail.com>
2022-10-05 17:50:26 -0300

    Commit

--- Personas/models.py
+++ Personas/models.py
@@ -5,4 +5,4 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     edad = models.IntegerField()
-    fecha_de_creacion = models.DateField(null = True)
+    fecha_de_nacimiento = models.IntegerField(null = True)
--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse
 from django.template import loader
 from datetime import datetime
 from Personas.models import Persona
+import random
 
 def ver_personas(request, nombre, apellido, edad):
-    persona = Persona(nombre = nombre, apellido = apellido, edad = edad, fecha_de_creacion = datetime.now())
+    persona = Persona(nombre = nombre, apellido = apellido, edad = edad, fecha_de_nacimiento = datetime.now(). year - edad)
+    persona.save()
     persona = Persona.objects.all()
-    
     template = loader.get_template('ver_personas.html')
     template_renderizado = template.render({'persona': persona})
     
